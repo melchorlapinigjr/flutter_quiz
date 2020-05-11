0e63f7442253d854c9af0eae99c064acca81ef5f
Melchor Lapinig Jr <lapinigmelchorjr@gmail.com>
2020-05-11 09:14:43 +0800

    added result score and reset quiz

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import './quiz.dart';
 import './result.dart';
 
-
 void main() => runApp(MyQuestionApp());
 
 class MyQuestionApp extends StatefulWidget {
@@ -15,51 +14,78 @@ class MyQuestionApp extends StatefulWidget {
 
 class _MyQuestionAppState extends State<MyQuestionApp> {
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  final _questions = const[
-      {
-        'questionText': 'What\'s your favorate color?',
-        'answer': ['Black', 'Red', 'Green', 'White'],
-      },
-      {
-        'questionText': 'What\'s your favorate animal?',
-        'answer': ['Dog', 'Cat', 'Pig', 'Cow'],
-      },
-      {
-        'questionText': 'What\'s your favorate city?',
-        'answer': ['Carmen', 'Batuan', 'Bilar', 'Langub'],
-      }, 
-      {
-        'questionText': 'Ang galing ni melchor?',
-        'answer': ['Tularan', 'Mo', 'Si', 'Melchor'],
-      },
-    ];
-
-  void _answerQuestion() {
-
-    if(_questionIndex < _questions.length){
-
-    }
+  void _resetQuiz() {
     setState(() {
-      _questionIndex = _questionIndex + 1; 
+      _questionIndex = 0;
+       _totalScore = 0;
+    });
+  }
+
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorate color?',
+      'answer': [
+        {'text': 'Black', 'score': 3},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 6},
+        {'text': 'White', 'score': 2},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorate animal?',
+      'answer': [
+        {'text': 'Dog', 'score': 6},
+        {'text': 'Cat', 'score': 8},
+        {'text': 'Pig', 'score': 3},
+        {'text': 'Cow', 'score': 3},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorate city?',
+      'answer': [
+        {'text': 'Carmen', 'score': 2},
+        {'text': 'Batuan', 'score': 3},
+        {'text': 'Bilar', 'score': 6},
+        {'text': 'Langub', 'score': 2},
+      ],
+    },
+    {
+      'questionText': 'Ang galing ni melchor?',
+      'answer': [
+        {'text': 'Tularan', 'score': 1},
+        {'text': 'Mo', 'score': 6},
+        {'text': 'Si', 'score': 4},
+        {'text': 'Melchor', 'score': 6},
+      ],
+    },
+  ];
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
+    if (_questionIndex < _questions.length) {}
+    setState(() {
+      _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-     
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: Text('Questions App'),
       ),
-      body: _questionIndex < _questions.length ? 
-      Quiz(
-        answerQuestion: _answerQuestion, 
-        questionIndex: _questionIndex, 
-        questions: _questions,) 
-      : Result(),
+      body: _questionIndex < _questions.length
+          ? Quiz(
+              answerQuestion: _answerQuestion,
+              questionIndex: _questionIndex,
+              questions: _questions,
+            )
+          : Result(_totalScore, _resetQuiz),
     ));
   }
 }
